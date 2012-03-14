68e3bd07032ad3f475a4ec054d5d714ab706e78c
Guayo <higgg.1989@gmail.com>
2012-03-13 19:01:44 -0600

    Principio tablasas

---
+++ tablas.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+struct var{
+	char* id;
+	int tipo;
+	int dv;
+};
+
+struct proc{
+	char* id;
+	int tipo;
+	struct var variables[100];
+};
+
+struct proc procedimientos[100];
+
+void agregaProcedimiento(int indexProc, char* id, int tipo){
+	procedimientos[indexProc].id=id;
+	procedimientos[indexProc].tipo=tipo;
+}
+
+void agregaVariable(int indexProc, int indexVar, char* id, int tipo, int dv){
+	procedimientos[indexProc].variables[indexVar].id=id;
+	procedimientos[indexProc].variables[indexVar].tipo=tipo;
+	procedimientos[indexProc].variables[indexVar].dv=dv;
+}
+
+int main(){
+	
+	
+	
+	
+	
+	procedimientos[0].id = "Primer Proc";
+	procedimientos[0].tipo = 0;
+	
+	procedimientos[0].variables[0].id = "primer variable";
+	procedimientos[0].variables[0].tipo = 0;
+	procedimientos[0].variables[0].dv=0;
+	
+	procedimientos[0].variables[1].id="Segunda variable";
+	procedimientos[0].variables[1].tipo=1;
+	procedimientos[0].variables[1].dv=1;
+	
+	procedimientos[1].id = "Segundo Proc";
+	procedimientos[1].tipo = 0;
+	
+	procedimientos[1].variables[0].id = "primer variable";
+	procedimientos[1].variables[0].tipo = 0;
+	procedimientos[1].variables[0].dv=0;
+	
+	procedimientos[1].variables[1].id="Segunda variable";
+	procedimientos[1].variables[1].tipo=1;
+	procedimientos[1].variables[1].dv=1;
+	
+	printf("primer proc, segunda variable, TIPO: %d", procedimientos[0].variables[1].tipo);
+
+	return 0;
+}
