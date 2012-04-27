703bd43eff11072854c4caaf778c13349752d9cf
Guayo <higgg.1989@gmail.com>
2012-04-27 01:32:41 -0500

    las cosas

--- MV/Cuadruplo.java
+++ MV/Cuadruplo.java
@@ -1,9 +1,14 @@
+/*La clase cuadruplo tiene como proposito facilitar el acceso a la informacion
+ *que contiene cada cuadruplo que fue generado por el compulador.*/
+
 public class Cuadruplo{
 	private int operacion;
 	private int operando1;
 	private int operando2;
 	private int resultado;
 	
+	/*Se utiliza un simple constructor que inicializa los valores del cuadruplo
+	 *segun lo que se leyo del archivo objeto.*/
 	public Cuadruplo(int operacion, int operando1, int operando2, int resultado){
 		this.operacion=operacion;
 		this.operando1=operando1;
@@ -11,6 +16,7 @@ public class Cuadruplo{
 		this.resultado=resultado;	
 	}
 	
+	// Metodos get usados para extraer la informacion de cada cuadruplo.
 	public int getOperacion(){
 		return operacion;
 	}
